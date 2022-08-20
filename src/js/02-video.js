86a993dc62fb933cb6580a621c86d30e06e75e58
Andrii <78295684+syniuk-a@users.noreply.github.com>
2022-08-20 16:00:28 +0300

    video page fixed

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,6 +1,6 @@
 // ! імпортуємо бібліотеки:
 // відео програвач
-import player, { Player } from "@vimeo/player";
+import Player from "@vimeo/player";
 // бібліотека затримки
 import throttle from "lodash.throttle";
 
@@ -10,8 +10,11 @@ const firstIframe = document.querySelector('#vimeo-player');
 const videoPlayer = new Player(firstIframe);
 
 // отримуємо поточну позицію
-const LOCALSTORAGE_KEY = 'videoplayer-current-time';
+const LOCALSTORAGE_KEY = localStorage.getItem('videoplayer-current-time');
 
+// записуємо в локальні поточну позицію
+const onPlay = videoPlayTime =>
+  localStorage.setItem('videoplayer-current-time', videoPlayTime.seconds);
 //  якщо поточна позиція після перезавантаження відмінна від "0", встановлюємо на плеєрі попередню позицію зупинки
 LOCALSTORAGE_KEY ? videoPlayer.setCurrentTime(LOCALSTORAGE_KEY) : null;
 
