ed15fc2bc591fd07e32579d061f993f6d89e307d
Andrii <78295684+syniuk-a@users.noreply.github.com>
2022-08-20 13:39:24 +0300

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,28 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+// описання в документації
+import simpleLightbox from 'simplelightbox';
+// додатковий імпорт стилів
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const gallaryContainer = document.querySelector('.gallary');
+// викликаємо функцію створення
+const gallaryItemMarkup = createGallaryItemMurkup(galleryItems);
+//створюємо функцію з макетом розмітки галереї
+function createGallaryItemMurkup(galleryImages) {
+  return galleryImages.map(({ original, preview, description }) => {
+    return `<div class="gallery__item">
+        <a href="${original}" class="gallery__link">
+          <img src="${preview}" alt="${description}" class="gallery__image">
+          </a>
+          </div>`}).join('');
+};
+// додаємо властивості
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionDelay: 200,
+  captionsData: 'alt',
+});
+// додаємо на сторінку
+gallaryContainer.insertAdjacentHTML('beforeend', gallaryItemMarkup);
